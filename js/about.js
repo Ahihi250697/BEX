da12c4659deadb57306bc23720525ae3f3a49e79
lanhsotret <58644190+lanhsotret@users.noreply.github.com>
2021-01-13 12:44:51 +0700

    add page about

---
+++ js/about.js
@@ -0,0 +1,59 @@
+function setflexOption(
+    eleTarget,
+    odd,
+    callback1,
+    callback2,
+    maxColumn,
+    gap
+  ) {
+    let _gap = gap || 0;
+    let _maxColumn = maxColumn || 0;
+    function loopEle(ele) {
+      let compareValue = Math.floor(
+        (ele.width() - _gap) / parseInt(ele.children().first().css("min-width"))
+      );
+      !_maxColumn ? _maxColumn = ele.children().length: '';
+      compareValue > _maxColumn ? (compareValue = _maxColumn) : compareValue;
+      !odd ? compareValue % 2 != 0? compareValue = compareValue - 1: compareValue: '';
+        let _callback1 =
+          callback1 ||
+          function () {
+              ele
+              .children()
+              .css(
+                "max-width",
+                "calc(100%/" + compareValue + " - " + _gap / compareValue + "px)"
+              );
+          };
+        let _callback2 =
+          callback2 ||
+          function () {
+            //   ele.children().css("max-width", "100%");
+            ele.addClass('trigger');
+          };
+        if (compareValue < _maxColumn) {
+            if(compareValue > 1) {
+              _callback1(ele, compareValue, _gap);
+            } else {
+              _callback2(ele, compareValue, _gap);
+            }
+        } else {
+        //   ele.children().attr("style", "");
+          ele.removeClass('trigger');
+        }
+    }
+    if (eleTarget.length > 1) {
+      $.each(eleTarget, function () {
+        let _parrent = $(this);
+        loopEle(_parrent);
+      });
+    } else {
+      loopEle(eleTarget);
+    }
+  }
+
+  setflexOption($(".about-list"), true, null, null, 2, 30);
+
+  window.addEventListener('resize', function() {
+    setflexOption($(".about-list"), true, null, null, 2, 30);
+});
